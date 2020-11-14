8fe87fa87162579c35990af3a23c5d8b469748c4
Owen Roth <rothowen27@gmail.com>
2020-11-13 20:53:52 -0600

    refractored code and env vars

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-
-    render() {
-        const {login, avatar_url, html_url} = this.props.user;
+const UserItem = ({user: {login, avatar_url, html_url}}) => {    
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="profile pic" className="round-img" style={{
@@ -17,7 +15,9 @@ class UserItem extends Component {
                 </div>
             </div>
         )
-    }
 }
 
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+}
 export default UserItem
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from "../layout/Spinner"
+import PropTypes from 'prop-types'
 
-export class Users extends Component {
-    state = {
-        users: [
-        {
-            id: '1',
-            login: 'Owen',
-            avatar_url: 'https://avatars1.githubusercontent.com/u/69156111?s=60&amp;v=4',
-            html_url: 'https://github.com/oroth8'
-        },
-        {
-            id: '2',
-            login: 'Dean',
-            avatar_url: 'https://avatars1.githubusercontent.com/u/69156111?s=60&amp;v=4',
-            html_url: 'https://github.com/oroth8'
-        },
-        {
-            id: '3',
-            login: 'Mike T',
-            avatar_url: 'https://avatars1.githubusercontent.com/u/69156111?s=60&amp;v=4',
-            html_url: 'https://github.com/oroth8'
-        },
-        ]
-    }
-    render() {
+
+const Users = ({users, loading}) =>{
+    if(loading){
+        return <Spinner/>
+    }else{
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
-        )
-    }
+        );
 }
+
+    }
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}  
 
 const userStyle = {
     display: 'grid',
