7c5e1f9cc28e85e103755f36ced3370257e6a369
Owen Roth <rothowen27@gmail.com>
2020-11-19 20:18:34 -0600

    edited env vars, added home page cleanup, and notfound page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, {useState, Fragment} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import About from './components/pages/About'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import Home from './components/pages/Home'
 import User from './components/users/User'
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState'
+import NotFound from './components/pages/NotFound'
 import './App.css';
 
 const App = () => {
@@ -20,16 +20,10 @@ const App = () => {
       <div className="container">
         <Alert/>
         <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search
-                />
-                <Users/>
-              </Fragment>
-            )}
-            />
+            <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About}/>
             <Route exact path="/user/:login" component={User}/>
+            <Route component={NotFound}/>
         </Switch>
       </div>
     </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p className='lead'>This page does note exist...</p>
+        </div>
+    )
+}
+
+export default NotFound;
